4402165d4e2cc0d29e1eb2ce267bcede9c24a061
vchistov_cp <SND\vchistov_cp>
2013-10-17 19:13:12 +0000

    Engine stub

---
+++ RefAssistant.Model/Engine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lardite.RefAssistant.Model.Projects;
+
+namespace Lardite.RefAssistant.Model
+{
+    public sealed class Engine
+    {
+        public Task<IEnumerable<VsProjectReference>> FindUnusedReferences(IVsProject project)
+        {
+            return new Task<IEnumerable<VsProjectReference>>(
+                () => Enumerable.Empty<VsProjectReference>());
+        }
+    }
+}
