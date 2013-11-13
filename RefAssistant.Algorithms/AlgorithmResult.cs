16b7adf45fd026fc68f1b08f087b2b00062f1f30
SergeyBelikov_cp <SND\SergeyBelikov_cp>
2013-11-13 22:18:32 +0000

    Add AssemblyManifestAlgorithm.

---
+++ RefAssistant.Algorithms/AlgorithmResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+using Lardite.RefAssistant.Algorithms.Data;
+
+namespace Lardite.RefAssistant.Algorithms
+{
+    internal sealed class AlgorithmResult
+    {
+        public string AlgorithmAdvice
+        {
+            get;
+            set;
+        }
+
+        public List<IAssembly> RequiredFor
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ RefAssistant.Algorithms/AssemblyManifestAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using Lardite.RefAssistant.Algorithms.Data;
+
+namespace Lardite.RefAssistant.Algorithms
+{
+    internal sealed class AssemblyManifestAlgorithm : IAlgorithm<IProject>
+    {
+        public AlgorithmResult Process(IProject project)
+        {
+            AlgorithmResult result = new AlgorithmResult();
+
+            //add all manifest assemblies
+            result.RequiredFor.AddRange(project.Assembly.References);
+
+            return result;
+        }
+    }
+}
