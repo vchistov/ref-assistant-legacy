521c5fbc058508c773d4e385155c834a6b6a7868
vchistov_cp <SND\vchistov_cp>
2014-01-10 21:18:02 +0000

    Getting Custom attributes of a type.

---
+++ RefAssistant.ReflectionServices/CecilExtensions/TypeDefinitionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mono.Cecil
+{
+    internal static class TypeDefinitionExtensions
+    {
+        /// <summary>
+        /// Get types definitions for modules collection.
+        /// </summary>
+        /// <param name="module">The modules definitions list.</param>
+        /// <returns>Returns list of types definitions.</returns>
+        public static IEnumerable<TypeDefinition> GetTypesDefinitions(this IEnumerable<ModuleDefinition> modules)
+        {
+            return modules.SelectMany(m => m.GetTypesDefinitions());
+        }
+
+        /// <summary>
+        /// Get types definitions for module.
+        /// </summary>
+        /// <param name="module">The module definition.</param>
+        /// <returns>Returns list of types definitions.</returns>
+        public static IEnumerable<TypeDefinition> GetTypesDefinitions(this ModuleDefinition module)
+        {
+            return module.Types.Union(module.Types.SelectMany(t => GetNestedTypes(t)));
+        }
+
+        #region Helpers
+        
+        private static IEnumerable<TypeDefinition> GetNestedTypes(TypeDefinition type)
+        {
+            if (type == null || !type.HasNestedTypes)
+            {
+                yield break;
+            }
+
+            foreach (var nestedType in type.NestedTypes)
+            {
+                yield return nestedType;
+                foreach (var innerNestedType in GetNestedTypes(nestedType))
+                {
+                    yield return innerNestedType;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- RefAssistant.ReflectionServices/CustomAttributeService.cs
+++ RefAssistant.ReflectionServices/CustomAttributeService.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using Lardite.RefAssistant.ReflectionServices.Data;
 using Lardite.RefAssistant.ReflectionServices.DataAccess.Containers;
+using Lardite.RefAssistant.ReflectionServices.DataAccess.Lookups;
 using Lardite.RefAssistant.ReflectionServices.DataAccess.Readers;
 using Mono.Cecil;
 
@@ -10,34 +11,47 @@ namespace Lardite.RefAssistant.ReflectionServices
     internal sealed class CustomAttributeService : ICustomAttributeService
     {
         private readonly IAssemblyContainer _container;
+        private readonly ITypeLookup _typeLookup;
 
         internal CustomAttributeService(IAssemblyContainer container)
         {
             ThrowUtils.ArgumentNull(() => container);
 
             _container = container;
+            _typeLookup = new TypeLookup(container);
         }
 
         IEnumerable<CustomAttributeInfo> ICustomAttributeService.GetAssemblyAttributes(AssemblyId assemblyId)
         {
             ThrowUtils.ArgumentNull(() => assemblyId);
 
-            var assembly = _container.Get(assemblyId);
-            foreach (var attribute in assembly.CustomAttributes)
-            {
-                yield return new CustomAttributeInfo(this.CreateReader(attribute));
-            }
+            return this.GetCustomAttributes(_container.Get(assemblyId));
         }
 
         IEnumerable<CustomAttributeInfo> ICustomAttributeService.GetTypeAttributes(TypeId typeId)
         {
-            // TODO: need to implement typecontainer?
-            throw new NotImplementedException();
+            ThrowUtils.ArgumentNull(() => typeId);
+
+            return this.GetCustomAttributes(_typeLookup.Get(typeId));
+        }
+
+        #region Helpers
+
+        private IEnumerable<CustomAttributeInfo> GetCustomAttributes(ICustomAttributeProvider entity)
+        {
+            Contract.Requires(entity != null);
+
+            foreach (var attribute in entity.CustomAttributes)
+            {
+                yield return new CustomAttributeInfo(this.CreateReader(attribute));
+            }
         }
 
         private ICustomAttributeReader CreateReader(CustomAttribute attribute)
         {
             return new CustomAttributeReader(attribute);
         }
+
+        #endregion
     }
 }
