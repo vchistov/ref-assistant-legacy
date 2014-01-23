d27c839cd8b218801e8c2adc2e522f73437c2730
vchistov_cp <SND\vchistov_cp>
2014-01-23 22:13:55 +0000

    CustomAttribute implementation

--- RefAssistant.Model/Processing/Data/CustomAttribute.cs
+++ RefAssistant.Model/Processing/Data/CustomAttribute.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Text;
 using Lardite.RefAssistant.Algorithms.Contracts;
 using Lardite.RefAssistant.ReflectionServices;
 using Lardite.RefAssistant.ReflectionServices.Data;
-using System.Diagnostics.Contracts;
 
 namespace Lardite.RefAssistant.Model.Processing.Data
 {
     internal sealed class CustomAttribute : ICustomAttribute
     {
-        private readonly IAssemblyService _assemblyService;
-        private readonly ITypeService _typeService;
+        private Lazy<ITypeDefinition> _attributeType;
+        private Lazy<IList<ITypeDefinition>> _ctorArguments;
+        private Lazy<IList<ITypeDefinition>> _fields;
+        private Lazy<IList<ITypeDefinition>> _properties;
 
         internal CustomAttribute(
             IAssemblyService assemblyService, 
@@ -25,28 +26,69 @@ namespace Lardite.RefAssistant.Model.Processing.Data
             Contract.Requires(customAttributeService != null);
             Contract.Requires(customAttributeInfo != null);
 
-            _assemblyService = assemblyService;
-            _typeService = typeService;
+            _attributeType = new Lazy<ITypeDefinition>(
+                () => new TypeDefinition(
+                    assemblyService, 
+                    typeService, 
+                    customAttributeService, 
+                    customAttributeInfo.AttributeType));
+
+            _ctorArguments = new Lazy<IList<ITypeDefinition>>(
+                () => LoadArgumentTypes(
+                    assemblyService,
+                    typeService,
+                    customAttributeService,
+                    customAttributeInfo.ConstructorArguments));
+
+            _fields = new Lazy<IList<ITypeDefinition>>(
+                () => LoadArgumentTypes(
+                    assemblyService,
+                    typeService,
+                    customAttributeService,
+                    customAttributeInfo.Fields));
+
+            _properties = new Lazy<IList<ITypeDefinition>>(
+                () => LoadArgumentTypes(
+                    assemblyService,
+                    typeService,
+                    customAttributeService,
+                    customAttributeInfo.Properties));
         }
 
         public ITypeDefinition AttributeType
         {
-            get { throw new NotImplementedException(); }
+            get { return _attributeType.Value; }
         }
 
-        public IEnumerable<ICustomAttributeArgument> ConstructorArguments
+        public IEnumerable<ITypeDefinition> ConstructorArguments
         {
-            get { throw new NotImplementedException(); }
+            get { return _ctorArguments.Value; }
         }
 
-        public IEnumerable<ICustomAttributeArgument> Fields
+        public IEnumerable<ITypeDefinition> Fields
         {
-            get { throw new NotImplementedException(); }
+            get { return _fields.Value; }
         }
 
-        public IEnumerable<ICustomAttributeArgument> Properties
+        public IEnumerable<ITypeDefinition> Properties
         {
-            get { throw new NotImplementedException(); }
+            get { return _properties.Value; }
         }
+
+        #region Helpers
+
+        private IList<ITypeDefinition> LoadArgumentTypes(
+            IAssemblyService assemblyService,
+            ITypeService typeService,
+            ICustomAttributeService customAttributeService,
+            IEnumerable<TypeId> typeIds)
+        {
+            return typeIds
+                .Select(id => new TypeDefinition(assemblyService, typeService, customAttributeService, id))
+                .Cast<ITypeDefinition>()
+                .ToList();
+        }
+
+        #endregion
     }
 }
